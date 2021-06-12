f29dceb7462425b7498588ea346671305bcc2e74
frederikho <frederik.holtel@gmx.net>
2021-06-12 11:50:24 +0200

    erstes script

---
+++ Assets/Scripts/p1Controller.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class p1Controller : MonoBehaviour
+{
+    public int PlayerNumberControls = 1; // determines weather player1 or player2 controls are used
+    private float leftright = 0f;
+    private float updown = 0f;
+    private string AxisName;   // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        if (PlayerNumberControls == 1) {
+            AxisName = "Player1";
+        }
+        if (PlayerNumberControls == 2) {
+            AxisName = "Player2";
+        }
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    leftright = Input.GetAxisRaw(AxisName+"Horizontal") * speed;
+    updown = Input.GetAxisRaw(AxisName+"Vertical") * speed; 
+        
+    }
+}   void FixedUpdate(){
+    if (leftright != 0 && updown != 0){
+            leftright *= limiter;
+            updown  *= limiter;
+        }
+    body.velocity = new Vector2(leftright * Time.fixedDeltaTime, updown * Time.fixedDeltaTime);
+}
