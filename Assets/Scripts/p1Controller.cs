410bcee099d4885f5f5382a497d78a083475acef
frederikho <frederik.holtel@gmx.net>
2021-06-12 12:48:05 +0200

    added gitignore

--- Assets/Scripts/p1Controller.cs
+++ Assets/Scripts/p1Controller.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class p1Controller : MonoBehaviour
 {
     public int PlayerNumberControls = 1; // determines weather player1 or player2 controls are used
-    private float leftright = 0f;
-    private float updown = 0f;
+    public float leftright = 0f;
+    public float updown = 0f;
     private string AxisName;   // Start is called before the first frame update
+    public float limiter = 0.8f;
+    public float speed = 200f;
+    Rigidbody2D body;
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
@@ -27,11 +30,12 @@ public class p1Controller : MonoBehaviour
     leftright = Input.GetAxisRaw(AxisName+"Horizontal") * speed;
     updown = Input.GetAxisRaw(AxisName+"Vertical") * speed; 
         
-    }
-}   void FixedUpdate(){
+    }   
+    void FixedUpdate() {
     if (leftright != 0 && updown != 0){
             leftright *= limiter;
             updown  *= limiter;
         }
     body.velocity = new Vector2(leftright * Time.fixedDeltaTime, updown * Time.fixedDeltaTime);
+    }
 }
